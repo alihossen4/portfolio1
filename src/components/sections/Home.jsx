913a86cf82c6c376b42c53a7c9df50162d487e4c
alihossen4 <mdahossen.dev@gmail.com>
2026-03-14 04:47:10 +0600

    style: home title changes

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -6,7 +6,7 @@ export const Home = () =>{
     >
       <div className="text-center z-10 px-4">
         <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent loading-right">
-          Hi, I'm Ali Tech
+          Hi, I'm Ali Hossan
         </h1>
         <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
           I'm a full-stack developer who loves crafting clean, scaleable web applications, My goal is to build solutions that offer both exceptional performance and a delightful user experience.
