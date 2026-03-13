14ac6658c6274bf4b6613c896ec3907e1bcf7484
alihossen4 <mdahossen.dev@gmail.com>
2026-03-14 04:43:25 +0600

    loadscreen updated

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -27,7 +27,6 @@ export const LoadingScreen = ({onComplete}) =>{
                 <div className="mb-4 text-4xl font-moono font-bold">{text}<span className="animate-ba ml-1"> | </span></div>
                 <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
                     <div className="h-full w-[40%] bg-blue-500 shadow-[0_0_15_#3b82f6] animate-loading-bar">
-                       { " "} 
                     </div>
                 </div>
             </div>
